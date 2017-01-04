6d6a3ee6f883632ff2af63d1879e6aeffc712d19
github username <github email address>
2017-01-04 17:28:26 -0500

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,19 @@
+class SecretsController < ApplicationController
+	before_action :authenticate_user
+
+	def show
+	end 	
+ 
+  private
+
+  def authenticate_user
+    if !logged_in?
+      flash[:notice] = 'You must be logged in to view this'
+      redirect_to '/login'
+    end
+  end
+
+  def logged_in?
+    !!session[:name]
+  end
+end 
